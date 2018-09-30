ea9c791f04585b5aa28ffbdec2254a985c93da36
Apua <Apua.A.Aa@gmail.com>
2018-09-30 23:14:35 +0800

    Convert value type when reading configuration

--- tinycal/tcal.py
+++ tinycal/tcal.py
@@ -9,17 +9,88 @@ from .config import TinyCalConfig
 from .render import TableYear, TableMonth, render
 
 
+def parse_config(number, line):
+    if '=' not in line:
+        raise Exception('...')
+
+    key, value = map(str.strip, line.split('=', 1))
+
+    if key in ('col', 'after', 'before'):
+        try:
+            value_ = int(value)
+        except ValueError:
+            raise ValueError(number)
+
+        if value_ < 0:
+            raise ValueError(number)
+
+        return key, value_
+
+    if key in ('start_monday', 'border', 'fill', 'sep', 'wk'):
+        if value.lower() not in ('true', 'false'):
+            raise ValueError(number)
+        return key, (value.lower() == 'true')
+
+    if key == 'lang':
+        if value not in ('en', 'jp', 'zh'):
+            raise ValueError(number)
+        return key, value
+
+    if key in (
+            'wk.color',
+            'fill.color',
+            'title.color',
+            'weekday.color',
+            'weekday.sunday.color',
+            'weekday.monday.color',
+            'weekday.tuesday.color',
+            'weekday.wednesday.color',
+            'weekday.thursday.color',
+            'weekday.friday.color',
+            'weekday.saturday.color',
+            'sunday.color',
+            'monday.color',
+            'tuesday.color',
+            'wednesday.color',
+            'thursday.color',
+            'friday.color',
+            'saturday.color',
+            'today.color',
+            ):
+        import re
+        patt = re.compile(r'^\s*(?P<fg>\w+)?\s*:?(?:\s*(?P<bg>\w+)\s*)?$')
+        if not patt.match(value):
+            raise ValueError(number)
+        return key, value
+
+    raise ValueError(number)
+
+
+def get_calrc():
+    """
+    :rtype: abs path | None
+    """
+    for rc in CALRC:
+        calrc = os.path.expanduser(rc)
+        if os.path.exists(rc):
+            return calrc
+
+
 def read_user_config():
     """
     :rtype: [(str, str)]
     """
-    try:
-        calrc = next(rc for rc in map(os.path.expanduser, CALRC) if os.path.exists(rc))
-    except StopIteration:
+    calrc = get_calrc()
+    if calrc is None:
         return []
-    else:
-        lines = filter(lambda l: l and not l.startswith('#'), map(str.strip, open(calrc)))
-        return [tuple(map(str.strip, l.split('=', 1))) for l in lines]
+
+    # read lines and strip
+    lines = ((n, l.strip()) for n, l in enumerate(open(calrc)))
+    # remove empty or commented lines
+    lines_ = ((n, l) for n, l in lines if l and not l.startswith('#'))
+    # parse configuration lines
+    config = (parse_config(n, l) for n, l in lines_)
+    return list(config)
 
 
 def get_command_arguments():
