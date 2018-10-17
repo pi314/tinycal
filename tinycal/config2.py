66c9e4a79b226357f8d676086e60b303e0c8af86
Apua Juan <apuaj@hpe.com>
2018-10-17 18:09:44 +0800

    Refactor `Color` class

--- tinycal/config2.py
+++ tinycal/config2.py
@@ -15,9 +15,9 @@ r"""
 >>> config.lang
 'en'
 >>> config.color_wk
-Color(BLACK, None)
+Color('BLACK:None')
 >>> config.color_today
-Color(None, white)
+Color('None:white')
 """
 
 import re
@@ -35,73 +35,112 @@ def greater_than(n):
             }
 
 
+def represent_color(fore, back):
+    assert isinstance(fore, Color)
+    assert isinstance(back, Color)
+
+    fg = fore.fg.upper() if fore.fg is not None and fore.highlight else fore.fg
+    bg = back.bg
+
+    return '%s:%s' % (fg, bg)
+
+
 class Color:
-    patt = re.compile(r'^\s*(?P<fg>\w+)?\s*:?(?:\s*(?P<bg>\w+)\s*)?$')
     definition = {
             'black': '0', 'red': '1', 'green': '2', 'yellow': '3',
             'blue': '4', 'magenta': '5', 'cyan': '6', 'white': '7',
             }
+    patt = re.compile(r'^\s*(?P<fg>\w+)?\s*:?(?:\s*(?P<bg>\w+)\s*)?$')
 
-    def __init__(self, *color_setting):
+    assert patt.match('').groups() == (None, None)
+    assert patt.match('a').groups() == ('a', None)
+    assert patt.match('a:').groups() == ('a', None)
+    assert patt.match(':b').groups() == (None, 'b')
+    assert patt.match('a:b').groups() == ('a', 'b')
+
+    def __init__(self, color_setting):
         r"""
-        >>> Color('a:b:c')
+        >>> Color('red:red:red')
         Traceback (most recent call last):
           ....
         ValueError
-        >>> Color('qwer')
+        """
+        m = self.patt.match(color_setting)
+        if m is None:
+            raise ValueError
+        self.highlight, self.fg, self.bg = self.clean(*m.groups())
+
+    def __bool__(self):
+        return False if self.fg == self.bg == None else True
+
+    def clean(self, fg, bg):
+        r"""
+        >>> Color('Apua')
         Traceback (most recent call last):
           ....
-        ValueError: Undefined color "qwer"
+        ValueError: unrecognized foreground color: Apua
         """
-        if len(color_setting) == 0:
-            self.fg = self.bg = None
-        elif len(color_setting) == 1:
-            m = self.patt.match(color_setting[0])
-            if m is None:
-                raise ValueError
-            self.fg, self.bg = map(self.clean, m.groups())
-        elif len(color_setting) == 2:
-            self.fg, self.bg = map(self.clean, color_setting)
+        if fg is None or fg.lower() == 'none':
+            fg_ = highlight = None
         else:
-            raise ValueError
+            fg_ = fg.lower()
+            highlight = (fg_.upper() == fg)
 
-    def clean(self, setting):
-        if setting is None or setting.lower() == 'none':
-            return None
+        if bg is None or bg.lower() == 'none':
+            bg_ = None
+        else:
+            bg_ = bg.lower()
 
-        if setting.lower() not in self.definition:
-            raise ValueError('Undefined color "%s"' % setting)
+        if fg_ is not None and fg_ not in self.definition:
+            raise ValueError('unrecognized foreground color: {}'.format(fg))
+        elif bg_ is not None and bg_ not in self.definition:
+            raise ValueError('unrecognized background color: {}'.format(bg))
 
-        return setting.upper() if setting == setting.upper() else setting.lower()
+        return highlight, fg_, bg_
+
+    def __str__(self):
+        r"""
+        >>> '%s' % Color('')
+        'None:None'
+        >>> '%s' % Color('BLACK:WHITE')
+        'BLACK:white'
+        """
+        return represent_color(self, self)
 
     def __lshift__(self, new):
         r"""
         >>> Color('BLACK:RED') << Color(':WHITE')
-        Color(BLACK, WHITE)
+        Color('BLACK:white')
         """
         assert isinstance(new, Color)
-        return Color(new.fg or self.fg, new.bg or self.bg)
+
+        fg_color = self if new.fg is None else new
+        bg_color = self if new.bg is None else new
+
+        return Color(represent_color(fg_color, bg_color))
 
     def __repr__(self):
         r"""
-        >>> str(Color('BLACK'))
-        'Color(BLACK, None)'
+        >>> Color('BLACK')
+        Color('BLACK:None')
+        >>> Color('')
+        Color('None:None')
         """
-        return '%s(%s, %s)' % (self.__class__.__name__, self.fg, self.bg)
+        return "%s('%s')" % (self.__class__.__name__, represent_color(self, self))
 
     def __call__(self, item):
         r"""
-        >>> Color()('raw string')
+        >>> Color('')('raw string')
         'raw string'
-        >>> Color(None,'white')(' * ')
+        >>> Color('none:white')(' * ')
         '\x1b[0;30;47m * \x1b[0m'
-        >>> Color(None,'cyan')(' * ')
+        >>> Color('none:cyan')(' * ')
         '\x1b[46m * \x1b[0m'
-        >>> Color('black',None)(' * ')
+        >>> Color('black:none')(' * ')
         '\x1b[0;30m * \x1b[0m'
-        >>> Color('BLACK',None)(' * ')
+        >>> Color('BLACK:none')(' * ')
         '\x1b[1;30m * \x1b[0m'
-        >>> Color('black','white')(' * ')
+        >>> Color('black:white')(' * ')
         '\x1b[0;30;47m * \x1b[0m'
         """
         code = self.code
@@ -125,7 +164,7 @@ class Color:
             else:
                 return code(bgcode(self.bg))  # keep foreground setting
         else:
-            bright = '%i' % (self.fg == self.fg.upper())
+            bright = '%i' % self.highlight
             if self.bg is None:
                 return code(bright, fgcode(self.fg))  # keep background setting
             else:
