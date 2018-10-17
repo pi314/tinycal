51e449734b1c396caa2fde495fc5cbfd12a6f07b
Apua Juan <apuaj@hpe.com>
2018-10-17 11:25:06 +0800

    Improve `Color` class

--- tinycal/config2.py
+++ tinycal/config2.py
@@ -15,10 +15,12 @@ r"""
 >>> config.lang
 'en'
 >>> config.color_wk
-Color(fg=BLACK,bg=None)
+Color(BLACK, None)
 >>> config.color_today
-Color(fg=None,bg=white)
+Color(None, white)
 """
+
+import re
 
 from .declarative_config import (
         Config, ValueField, ValidationError,
@@ -34,29 +36,100 @@ def greater_than(n):
 
 
 class Color:
+    patt = re.compile(r'^\s*(?P<fg>\w+)?\s*:?(?:\s*(?P<bg>\w+)\s*)?$')
     definition = {
             'black': '0', 'red': '1', 'green': '2', 'yellow': '3',
             'blue': '4', 'magenta': '5', 'cyan': '6', 'white': '7',
             }
 
-    def __init__(self, color_setting):
-        import re
-        patt = re.compile(r'^\s*(?P<fg>\w+)?\s*:?(?:\s*(?P<bg>\w+)\s*)?$')
-        m = patt.match(color_setting)
-        if m is None:
+    def __init__(self, *color_setting):
+        r"""
+        >>> Color('a:b:c')
+        Traceback (most recent call last):
+          ....
+        ValueError
+        >>> Color('qwer')
+        Traceback (most recent call last):
+          ....
+        ValueError: Undefined color "qwer"
+        """
+        if len(color_setting) == 0:
+            self.fg = self.bg = None
+        elif len(color_setting) == 1:
+            m = self.patt.match(color_setting[0])
+            if m is None:
+                raise ValueError
+            self.fg, self.bg = map(self.clean, m.groups())
+        elif len(color_setting) == 2:
+            self.fg, self.bg = map(self.clean, color_setting)
+        else:
             raise ValueError
-        self.fg = self.clean(m.group('fg'))
-        self.bg = self.clean(m.group('bg'))
 
     def clean(self, setting):
         if setting is None or setting.lower() == 'none':
             return None
+
         if setting.lower() not in self.definition:
-            raise ValueError('Not defined color setting %s' % setting)
-        return setting if setting == setting.upper() else setting.lower()
+            raise ValueError('Undefined color "%s"' % setting)
+
+        return setting.upper() if setting == setting.upper() else setting.lower()
+
+    def __lshift__(self, new):
+        r"""
+        >>> Color('BLACK:RED') << Color(':WHITE')
+        Color(BLACK, WHITE)
+        """
+        assert isinstance(new, Color)
+        return Color(new.fg or self.fg, new.bg or self.bg)
 
     def __repr__(self):
-        return '%s(fg=%s,bg=%s)' % (self.__class__.__name__, self.fg, self.bg)
+        r"""
+        >>> str(Color('BLACK'))
+        'Color(BLACK, None)'
+        """
+        return '%s(%s, %s)' % (self.__class__.__name__, self.fg, self.bg)
+
+    def __call__(self, item):
+        r"""
+        >>> Color()('raw string')
+        'raw string'
+        >>> Color(None,'white')(' * ')
+        '\x1b[0;30;47m * \x1b[0m'
+        >>> Color(None,'cyan')(' * ')
+        '\x1b[46m * \x1b[0m'
+        >>> Color('black',None)(' * ')
+        '\x1b[0;30m * \x1b[0m'
+        >>> Color('BLACK',None)(' * ')
+        '\x1b[1;30m * \x1b[0m'
+        >>> Color('black','white')(' * ')
+        '\x1b[0;30;47m * \x1b[0m'
+        """
+        code = self.code
+        reset = '\033[0m'
+        if code:
+            return '%s%s%s' % (code, item, reset)
+        else:
+            return '%s' % item
+
+    @property
+    def code(self):
+        fgcode = lambda c: '3%s' % self.definition[c.lower()]
+        bgcode = lambda c: '4%s' % self.definition[c]
+        code = lambda *t: '\033[%sm' % ";".join(t)
+
+        if self.fg is None:
+            if self.bg is None:
+                return ''
+            elif self.bg == 'white':
+                return code('0', fgcode('black'), bgcode('white'))  # reverse
+            else:
+                return code(bgcode(self.bg))  # keep foreground setting
+        else:
+            bright = '%i' % (self.fg == self.fg.upper())
+            if self.bg is None:
+                return code(bright, fgcode(self.fg))  # keep background setting
+            else:
+                return code(bright, fgcode(self.fg), bgcode(self.bg))
 
 
 class ColorField(ValueField):
