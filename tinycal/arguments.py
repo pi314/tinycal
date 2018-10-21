914969dca65b5f5f37987be168e57c1d90f455c9
Apua <Apua.A.Aa@gmail.com>
2018-10-21 18:18:21 +0800

    Recover `argparse` code to clear the diff

--- tinycal/arguments.py
+++ tinycal/arguments.py
@@ -23,49 +23,49 @@ parser.add_argument('-A', dest='after', default=None, type=int,
 parser.add_argument('-B', dest='before', default=None, type=int,
                     help='Display the number of months before the current month.')
 
-parser.add_argument('-3', dest='a1b1', action='store_true', default=None,
+parser.add_argument('-3', action='store_true', dest='a1b1', default=None,
                     help='Equals to -A 1 -B 1.')
 
-parser.add_argument('-w', dest='wk', action='store_true', default=None,
+parser.add_argument('-w', action='store_true', dest='wk', default=None,
                     help='Display week number.')
-parser.add_argument('-W', dest='wk', action='store_false', default=None,
+parser.add_argument('-W', action='store_false', dest='wk', default=None,
                     help='Don`t display week number.')
 
-parser.add_argument('-s', '--sep', dest='sep', action='store_true', default=None,
+parser.add_argument('-s', '--sep', action='store_true', dest='sep', default=None,
                     help='Display separation lines.')
-parser.add_argument('-S', '--no-sep', dest='sep', action='store_false', default=None,
+parser.add_argument('-S', '--no-sep', action='store_false', dest='sep', default=None,
                     help='Don`t display separation lines.')
 
-parser.add_argument('-b', '--border', dest='border', action='store_true', default=None,
+parser.add_argument('-b', '--border', action='store_true', dest='border', default=None,
                     help='Display border lines.')
-parser.add_argument('-nb', '--no-border', dest='border', action='store_false', default=None,
+parser.add_argument('-nb', '--no-border', action='store_false', dest='border', default=None,
                     help='Don`t display border lines.')
 
-parser.add_argument('-f', '--fill', dest='fill', action='store_true', default=None,
+parser.add_argument('-f', '--fill', action='store_true', dest='fill', default=None,
                     help='Fill every month into rectangle with previous/next month dates.')
-parser.add_argument('-F', '--no-fill', dest='fill', action='store_false', default=None,
+parser.add_argument('-F', '--no-fill', action='store_false', dest='fill', default=None,
                     help='Don`t fill month into rectangle.')
 
-parser.add_argument('-c', dest='color', action='store_true', default=True,
+parser.add_argument('-c', action='store_true', dest='color', default=True,
                     help='Enable VT100 color output.')
-parser.add_argument('-C', dest='color', action='store_false', default=True,
+parser.add_argument('-C', action='store_false', dest='color', default=True,
                     help='Disable VT100 color output.')
 
-parser.add_argument('-l', '--lang', dest='lang', choices=['jp', 'zh', 'en'], type=str,
+parser.add_argument('-l', '--lang', choices=['jp', 'zh', 'en'], type=str,
                     help='Select the language used to display weekday.')
 
-parser.add_argument('-j', dest='lang',action='store_const', const='jp',
+parser.add_argument('-j', action='store_const', const='jp', dest='lang',
                     help='Enable Japanese weekday names, equals to --lang=jp.')
 
-parser.add_argument('-z', dest='lang',action='store_const', const='zh',
+parser.add_argument('-z', action='store_const', const='zh', dest='lang',
                     help='Enable Chinese weekday names, equals to --lang=zh.')
 
-parser.add_argument('-e', dest='lang',action='store_const', const='en',
+parser.add_argument('-e', action='store_const', const='en', dest='lang',
                     help='Enable Chinese weekday names, equals to --lang=en.')
 
-parser.add_argument('-m', dest='start_monday', action='store_true', default=None,
+parser.add_argument('-m', action='store_true', dest='start_monday', default=None,
                     help='Use Monday as first weekday.')
-parser.add_argument('-M', dest='start_monday', action='store_false', default=None,
+parser.add_argument('-M', action='store_false', dest='start_monday', default=None,
                     help='Use Sunday as first weekday.')
 
 parser.add_argument('year', type=int, nargs='?', default=None,
